fcd75d941c055b1fb4fff5adc4b5a67fb2d401c3
Antor <contact@antor.fr>
2025-03-02 16:38:41 +0100

    Update tx_ultimate_touch.cpp

--- components/tx_ultimate_touch/tx_ultimate_touch.cpp
+++ components/tx_ultimate_touch/tx_ultimate_touch.cpp
@@ -192,10 +192,10 @@ namespace esphome
 
         TouchPoint TxUltimateTouch::get_touch_point(int bytes[])
         {
-            ESP_LOGV("UART-Log", "------------");
+            ESP_LOGI("UART-Log", "------------");
             for (int i = 4; i < 15; i++)
             {
-                ESP_LOGV("UART-Log", "%d: %i", i, bytes[i]);
+                ESP_LOGI("UART-Log", "%d: %i", i, bytes[i]);
             }
             
             TouchPoint tp;
